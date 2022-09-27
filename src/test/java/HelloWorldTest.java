b8ecdfdb7ca820c53bf34bf603352664c9143299
dolgikhaa@rtinfosys.ru <MlxkVKM9ic>
2022-09-27 16:02:12 +0300

    Read JSON

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,16 +1,25 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HelloWorldTest {
 
     @Test
-    public void testHelloWorld() {
-        Response res = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
-                .andReturn();
+    public void testRestAssured() {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "John");
 
-        System.out.println("Text from body: " + res.prettyPrint());
+        JsonPath resp = RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        System.out.println("Весь JSON: " + resp.prettyPrint());
     }
 }
