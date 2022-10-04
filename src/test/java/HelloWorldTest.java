4758bac93f228520fe7700e383419e3dcd01e42a
dolgikhaa@rtinfosys.ru <MlxkVKM9ic>
2022-10-05 00:53:51 +0300

    Redirect URL

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,27 +1,22 @@
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class HelloWorldTest {
 
     @Test
     public void testRestAssured() {
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "John");
+        String mainURL = "http://playground.learnqa.ru/api/";
 
         Response resp = RestAssured
                 .given()
-                .queryParams(params)
-                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .redirects()
+                .follow(false)
+                .when()
+                .get(mainURL + "long_redirect")
                 .andReturn();
 
-        String message1 = resp.jsonPath().getString("messages[1].message");
-
-        System.out.println("Второе сообщение из JSON: " + message1);
+        String locationHeader = resp.getHeader("Location");
+        System.out.println("Redirect: " + locationHeader);
     }
 }
